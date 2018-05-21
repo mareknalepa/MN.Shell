a5d8a58e41c3829c6b6f03fc56248147080d4b73
Marek Nalepa <marek.nalepa@gmail.com>
2018-05-21 23:13:40 +0200

    Configure Caliburn.Micro framework

---
+++ src/MN.Shell.Core/Modules/MainWindow/MainWindowViewModel.cs
@@ -0,0 +1,15 @@
+﻿using MN.Shell.Core.Framework;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MN.Shell.Core.Modules.MainWindow
+{
+    [Export(typeof(IShell))]
+    public class MainWindowViewModel : IShell
+    {
+    }
+}
