1f46d8322e76c387233dc5029e3573fa0ec7d4f1
Marek Nalepa <marek.nalepa@gmail.com>
2018-12-16 17:52:08 +0100

    Add DialogButtons control

---
+++ src/MN.Shell.Core/Controls/DialogButtons.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace MN.Shell.Core.Controls
+{
+    public class DialogButtons : ItemsControl
+    {
+        static DialogButtons()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(DialogButtons),
+                new FrameworkPropertyMetadata(typeof(DialogButtons)));
+        }
+    }
+}
