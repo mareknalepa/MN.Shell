02173bcc0528d0af6dcf0127df32f9fee7c65d26
Marek Nalepa <marek.nalepa@gmail.com>
2019-03-10 21:36:15 +0100

    Add basic empty menu components

---
+++ src/MN.Shell/Framework/FrameworkModule.cs
@@ -0,0 +1,15 @@
+﻿using MN.Shell.Framework.Menu;
+using Ninject.Modules;
+
+namespace MN.Shell.Framework
+{
+    public class FrameworkModule : NinjectModule
+    {
+        public override string Name => "MN.Shell.Framework";
+
+        public override void Load()
+        {
+            Bind<IMenuAggregator>().To<MenuAggregator>();
+        }
+    }
+}
