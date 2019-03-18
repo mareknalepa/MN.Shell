663f658e4d33b355f0c0a5eb74ea431cb4e48309
Marek Nalepa <marek.nalepa@gmail.com>
2019-03-18 21:39:29 +0100

    Add basic Bootstrapper test

---
+++ src/MN.Shell.Tests/Core/BootstrapperTests.cs
@@ -0,0 +1,55 @@
+﻿using MN.Shell.Core;
+using NUnit.Framework;
+using System;
+using System.Threading;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace MN.Shell.Tests.Core
+{
+    [TestFixture]
+    public class BootstrapperTests
+    {
+        private Thread _fakeUiThread;
+
+        [Test]
+        [Timeout(5000)]
+        public void BootstrapperInitTest()
+        {
+            ManualResetEventSlim manualResetEventSlim = new ManualResetEventSlim();
+            Application app = null;
+
+            _fakeUiThread = new Thread(() =>
+            {
+                app = new Application();
+                Bootstrapper bootstrapper = new Bootstrapper();
+
+                app.Dispatcher.Invoke(DispatcherPriority.Normal, new Action(() =>
+                {
+                    manualResetEventSlim.Set();
+                }));
+
+                app.Dispatcher.BeginInvokeShutdown(DispatcherPriority.Background);
+
+                app.Run();
+            });
+
+            _fakeUiThread.SetApartmentState(ApartmentState.STA);
+            _fakeUiThread.Start();
+
+            manualResetEventSlim.Wait();
+
+            Assert.NotNull(app);
+            Assert.NotNull(app.Dispatcher);
+
+            app.Dispatcher.BeginInvokeShutdown(DispatcherPriority.Background);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            if (!_fakeUiThread?.Join(500) ?? false)
+                _fakeUiThread?.Abort();
+        }
+    }
+}
---
+++ src/MN.Shell.Tests/Mocks/MockModule.cs
@@ -0,0 +1,15 @@
+﻿using MN.Shell.Core;
+using Ninject.Modules;
+
+namespace MN.Shell.Tests.Mocks
+{
+    public class MockModule : NinjectModule
+    {
+        public override string Name => "MN.Shell.Tests.Mock";
+
+        public override void Load()
+        {
+            Rebind<IShell>().To<MockShell>();
+        }
+    }
+}
