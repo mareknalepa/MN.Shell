5985c96af42e9b66692dff6a07d0327119ac106a
Marek Nalepa <marek.nalepa@gmail.com>
2019-03-18 18:48:51 +0100

    Add empty unit tests project

---
+++ src/MN.Shell.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MN.Shell.Tests")]
+[assembly: AssemblyDescription("MN.Shell core components tests")]
+[assembly: AssemblyCulture("")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("097e4c4f-7283-4f92-a1d7-5db8b65f4975")]
