739f8482881d3f990f46d0e2cdcbc26a87afe8a7
Marek Nalepa <marek.nalepa@gmail.com>
2019-03-20 20:37:45 +0100

    Add WindowManager tests

---
+++ src/MN.Shell.Tests/Core/WindowManagerTests.cs
@@ -0,0 +1,37 @@
+﻿using MN.Shell.Tests.Mocks;
+using NUnit.Framework;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MN.Shell.Tests.Core
+{
+    [TestFixture]
+    [Apartment(ApartmentState.STA)]
+    public class WindowManagerTests
+    {
+        [Test]
+        public void WindowManagerEnsureWindowForWindowTest()
+        {
+            MockWindowManager windowManager = new MockWindowManager();
+
+            var vm = new MockWindowViewModel();
+            Window window = new MockWindowView();
+            var view = windowManager.EnsureWindow(vm, window, false);
+
+            Assert.AreSame(window, view);
+        }
+
+        [Test]
+        public void WindowManagerEnsureWindowForUserControlTest()
+        {
+            MockWindowManager windowManager = new MockWindowManager();
+
+            var vm = new MockUserControlViewModel();
+            UserControl userControl = new MockUserControlView();
+            var view = windowManager.EnsureWindow(vm, userControl, false);
+
+            Assert.AreSame(userControl, view.Content);
+        }
+    }
+}
---
+++ src/MN.Shell.Tests/Mocks/MockWindowManager.cs
@@ -0,0 +1,11 @@
+﻿using MN.Shell.Core;
+using System.Windows;
+
+namespace MN.Shell.Tests.Mocks
+{
+    public class MockWindowManager : WindowManager
+    {
+        public new Window EnsureWindow(object model, object view, bool isDialog) =>
+            base.EnsureWindow(model, view, isDialog);
+    }
+}
