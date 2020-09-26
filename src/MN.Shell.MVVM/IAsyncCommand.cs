87e06e0958d2de48b7ce7a2caac60c0a40376f66
Marek Nalepa <marek.nalepa@gmail.com>
2020-09-26 15:13:01 +0200

    Add IAsyncCommand

---
+++ src/MN.Shell.MVVM/IAsyncCommand.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MN.Shell.MVVM
+{
+    /// <summary>
+    /// Defines command which can be executed asynchronously
+    /// </summary>
+    public interface IAsyncCommand : ICommand
+    {
+        /// <summary>
+        /// Executes command asynchronously
+        /// </summary>
+        /// <param name="parameter">Internal parameter which can be optionally passed to command</param>
+        /// <returns>Task representing asynchronous operation</returns>
+        Task ExecuteAsync(object parameter);
+    }
+}
